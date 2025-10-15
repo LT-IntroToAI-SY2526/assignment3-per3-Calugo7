852cf53a47276a7b41719291776cd0facac56d9e
Caleb Lugo <clugo1@cps.edu>
2025-10-15 10:58:09 -0500

    Finished a3

--- a3.py
+++ a3.py
@@ -251,10 +251,8 @@ def search_pa_list(src: List[str]) -> List[str]:
     """Takes source, finds matching pattern and calls corresponding action. If it finds
     a match but has no answers it returns ["No answers"]. If it finds no match it
     returns ["I don't understand"].
-
     Args:
         source - a phrase represented as a list of words (strings)
-
     Returns:
         a list of answers. Will be ["I don't understand"] if it finds no matches and
         ["No answers"] if it finds a match but no answers
